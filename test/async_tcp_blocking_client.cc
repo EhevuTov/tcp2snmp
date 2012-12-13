74d1319f3a87131be012fd892c0ee9676e145ba5
James Gosnell <jamesgosnell@gmail.com>
2012-12-13 16:50:06 -0600

    ADDED direction field to test

--- test/async_tcp_blocking_client.cc
+++ test/async_tcp_blocking_client.cc
@@ -1,45 +1,28 @@
-#include <cstdlib>
-#include <cstring>
 #include <iostream>
 #include <boost/asio.hpp>
-//#include <boost/test/unit_test.hpp>
-
-//#define BOOST_TEST_MODULE const_string test
+#include <boost/asio/buffer.hpp>
+#include <boost/algorithm/string.hpp>
 
 using boost::asio::ip::tcp;
 
-enum { max_length = 1024 };
+enum { max_length = 160 };
 
-char signature[] = "\x53\x4e\x4d\x50";
-char length[] = "\xA4\x34\xA1\x32";
-char event[] = "\x02\x01\x02\x02\x02\x03\xe8 \
-                \x16\x06\x0b\x4c\x45\x4e\x58";
-
-class msg {
-  public:
-    msg();
-    msg(char* buff){
-      unpack(buff);
-      std::cout << "successful\n";
-      std::cout.write(length,4);
-    };
-  private:
-    int pack();
-    int unpack(char* buff){
-      printf("%s\n",buff);
-      return 1;
-    };
+struct data {
+  char signature[6];
+  char eventType[2];
+  char teknoID[6];
+  char linkSet[18];
+  char linkQual[56];
+  char origin[18];
+  char destin[18];
+  char threshold[4];
+  char occupancy[4];
+  char direction[1];
+  char padding[27];
 };
-class pdu {
-  public:
-    pdu();
-    int send();
-    int close();
 
-  private:
-    int pack();
-    int unpack();
-};
+typedef struct data Data;
+unsigned char* buffer[160];
 
 int main(int argc, char* argv[])
 {
@@ -60,29 +43,32 @@ int main(int argc, char* argv[])
     tcp::socket s(io_service);
     boost::asio::connect(s, iterator);
 
-    using namespace std; // For strlen.
-    cout << "Enter message: ";
-    char request[max_length];
-    std::cin.getline(request, max_length);
-    size_t request_length = strlen(request);
-    boost::asio::write(s, boost::asio::buffer(request, request_length));
+    using namespace std;
 
-    char reply[max_length];
-    size_t reply_length = boost::asio::read(s,
-        boost::asio::buffer(reply, request_length));
-    std::cout << "Reply is: ";
-    std::cout.write(reply, reply_length);
-    std::cout << "\n";
-    msg msg(reply);
-/*
-    if(msg tcp_msg(reply)){
-      exit(1);
-    };
-*/
+    Data* request;
+    request=(Data*)malloc(sizeof(Data));
+    memset(request, '\0', sizeof(Data));
+    if (NULL == request) {return 0;};
+
+    strcpy(request->signature, "SNMP");
+    strcpy(request->eventType, "1");
+    strcpy(request->teknoID, "1000");
+    strcpy(request->linkSet, "LS0001");
+    strcpy(request->linkQual, "6");
+    strcpy(request->origin, "LENXPA671AD");
+    strcpy(request->destin, "LENXPA782BF");
+    strcpy(request->threshold, "60");
+    strcpy(request->occupancy, "70");
+    strcpy(request->direction, "1");
+
+    size_t request_length = sizeof(Data);
+
+    boost::asio::write(s, boost::asio::buffer((void*)request, request_length));
+
   }
   catch (std::exception& e)
   {
-//    std::cerr << "Exception: " << e.what() << "\n";
+    std::cerr << "Exception: " << e.what() << "\n";
   }
   return 0;
 }
